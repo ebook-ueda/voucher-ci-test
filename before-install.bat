75324b9358c07ab35d24d0efac40e6de3d9f20a4
ebook-ueda <takeshi.ueda@ebookjapan.net>
2017-12-11 23:39:11 +0900

    before-install.batの削除パスの誤りを修正

--- before-install.bat
+++ before-install.bat
@@ -1,7 +1,7 @@
 REM Install Internet Information Server (IIS).
 REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-mkdir c:\inetpub\HelloWebAPI > NUL 2>&1
-del      /q c:\inetpub\HelloWebAPI\src\HelloWebAPI\Web.config
-del      /q c:\inetpub\HelloWebAPI\src\HelloWebAPI\Global.asax
-rmdir /s /q c:\inetpub\HelloWebAPI\src\HelloWebAPI\bin
+mkdir c:\inetpub\HelloWebAPI                   > NUL 2>&1
+del      /q c:\inetpub\HelloWebAPI\Web.config  > NUL 2>&1
+del      /q c:\inetpub\HelloWebAPI\Global.asax > NUL 2>&1
+rmdir /s /q c:\inetpub\HelloWebAPI\bin         > NUL 2>&1
