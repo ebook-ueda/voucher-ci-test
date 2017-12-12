434e2a39629c336713916138bb23937a8d3585c4
ebook-ueda <takeshi.ueda@ebookjapan.net>
2017-12-12 15:57:26 +0900

    buildspec.ymlを作成

--- before-install.bat
+++ before-install.bat
@@ -1,6 +1,3 @@
-REM Install Internet Information Server (IIS).
-REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
 mkdir c:\inetpub\HelloWebAPI     > NUL 2>&1
 if exist c:\inetpub\HelloWebAPI\Web.config  del      /q c:\inetpub\HelloWebAPI\Web.config
 if exist c:\inetpub\HelloWebAPI\Global.asax del      /q c:\inetpub\HelloWebAPI\Global.asax
